a1367eb0308303ddd9e076dc624a5e8ae666990d
AngelGallardo <angelgallardoturpin@gmail.com>
2024-11-05 12:27:50 +0100

    productos dto

---
+++ Modelo/ProductosDTO.php
@@ -0,0 +1,73 @@
+<?php
+
+Class ProductosDTO{
+    private $id;
+    private $nombre;
+    private $precio;
+    private $descripcion;
+    private $cliente_id;
+
+    public function __construct($id, $precio, $nombre, $descripcion, $cliente_id)
+    {
+        $this->id = $id;
+        $this->precio = $precio;
+        $this->nombre = $nombre;
+        $this->descripcion = $descripcion;
+        $this->cliente_id = $cliente_id;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    public function setPrecio($precio)
+    {
+        $this->precio = $precio;
+    }
+
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+    }
+
+    public function getClienteId()
+    {
+        return $this->cliente_id;
+    }
+
+    public function setClienteId($cliente_id)
+    {
+        $this->cliente_id = $cliente_id;
+    }
+
+
+
+
+}
+?>
