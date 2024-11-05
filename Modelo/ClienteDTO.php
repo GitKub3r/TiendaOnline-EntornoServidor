c2c9c61a0e9a13905dab32cd12e68dba83f78181
= <=>
2024-11-05 12:26:31 +0100

    Añadido ClienteDTO.php

---
+++ Modelo/ClienteDTO.php
@@ -0,0 +1,88 @@
+<?php
+
+class ClienteDTO
+{
+    private $id;
+    private $nombre;
+    private $apellido;
+    private $nickname;
+    private $password;
+    private $telefono;
+    private $domicilio;
+
+    public function __construct($nickname, $password)
+    {
+        $this->nickname = $nickname;
+        $this->password = $password;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    public function setApellido($apellido)
+    {
+        $this->apellido = $apellido;
+    }
+
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+    public function setTelefono($telefono)
+    {
+        $this->telefono = $telefono;
+    }
+
+    public function getDomicilio()
+    {
+        return $this->domicilio;
+    }
+
+    public function setDomicilio($domicilio)
+    {
+        $this->domicilio = $domicilio;
+    }
+
+    public function getNickname()
+    {
+        return $this->nickname;
+    }
+
+    public function setNickname($nickname)
+    {
+        $this->nickname = $nickname;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+}
