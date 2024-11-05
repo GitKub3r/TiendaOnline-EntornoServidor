d0b74e85db135aed30cd8726e31b1cb2ecc81fce
AngelGallardo <angelgallardoturpin@gmail.com>
2024-11-05 10:51:09 +0100

    conexion base de datos

--- Modelo/database.php
+++ Modelo/database.php
@@ -1,8 +1,21 @@
 <?php
 
-namespace Modelo;
-
 class database
 {
-
+    private static $servername = "localhost:3306";
+    private static $username = "root";
+    private static $password = "";
+    private static $dbname = "pruebas";
+    private static $conn = null;
+    public static function getConnection() {
+        if (self::$conn === null) {
+            try {
+                self::$conn = new PDO("mysql:host=" . self::$servername . ";dbname=" . self::$dbname, self::$username, self::$password);
+                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                echo "Error de conexión: " . $e->getMessage();
+            }
+        }
+        return self::$conn;
+    }
 }
