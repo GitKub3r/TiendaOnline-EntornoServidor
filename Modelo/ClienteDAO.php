3e6499b39a0266eac88020d9d39f9c46e0d44b26
= <=>
2024-11-06 10:44:45 +0100

    Añadido métodos CRUD ClienteDAO.php

---
+++ Modelo/ClienteDAO.php
@@ -0,0 +1,86 @@
+<?php
+
+require_once "Database.php";
+require_once "Cliente.php";
+
+class ClienteDAO
+{
+    private $connection;
+    public function __construct() {
+        $this->connection = Database::getConnection();
+    }
+
+    public function getClienteByID($id) {
+        $stmt = $this->connection->prepare("SELECT * FROM cliente WHERE id = :id");
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+
+        $fila = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($fila) {
+            $cliente = new ClienteDTO($fila["nickname"], $fila["password"]);
+            $cliente->setNombre($fila["nombre"]);
+            $cliente->setApellido($fila["apellido"]);
+            $cliente->setTelefono($fila["telefono"]);
+            $cliente->setDomicilio($fila["domicilio"]);
+            return $cliente;
+        } else {
+            return null;
+        }
+    }
+
+    public function getClienteByNickname($nickname, $password) {
+        $stmt = $this->connection->prepare("SELECT * FROM cliente WHERE nickname = :nickname");
+
+        $stmt->bindParam(":nickname", $nickname);
+        $stmt->execute();
+
+        $fila = $stmt->fetch(PDO::FETCH_ASSOC);
+        $sha1 = sha1($password);
+
+        if ($fila) {
+            if ($fila["password"] == $sha1) {
+                $cliente = new ClienteDTO($fila["nickname"], $fila["password"]);
+                $cliente->setId($fila["id"]);
+
+                return $cliente;
+            } else {
+                $cliente = new ClienteDTO($fila["nickname"], null);
+                return $cliente;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public function addCliente($cliente) {
+        $stmt = $this->connection->prepare("INSERT INTO cliente (nombre, apellido, nickname, password, telefono, domicilio) VALUES (:nombre, :apellido, :nickname, :password, :telefono, :domicilio)");
+
+        $stmt->bindParam(":nombre", $cliente->getNombre());
+        $stmt->bindParam(":apellido", $cliente->getApellido());
+        $stmt->bindParam(":nickname", $cliente->getNickname());
+        $stmt->bindParam(":password", $cliente->getPassword());
+        $stmt->bindParam(":telefono", $cliente->getTelefono());
+        $stmt->bindParam(":domicilio", $cliente->getDomicilio());
+        $stmt->execute();
+    }
+
+    public function updateCliente($cliente) {
+        $stmt = $this->connection->prepare("UPDATE clientes SET nombre = :nombre, apellido = :apellido, nickname = :nickname, password = :password, telefono = :telefono, domicilio = :domicilio WHERE id = :id");
+
+        $stmt->bindParam(":nombre", $cliente->getNombre());
+        $stmt->bindParam(":apellido", $cliente->getApellido());
+        $stmt->bindParam(":nickname", $cliente->getNickname());
+        $stmt->bindParam(":password", $cliente->getPassword());
+        $stmt->bindParam(":telefono", $cliente->getTelefono());
+        $stmt->bindParam(":domicilio", $cliente->getDomicilio());
+        $stmt->bindParam(":id", $cliente->getId());
+        $stmt->execute();
+    }
+
+    public function deleteCliente($id) {
+        $stmt = $this->connection->prepare("DELETE FROM cliente WHERE id = :id");
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+    }
+}
